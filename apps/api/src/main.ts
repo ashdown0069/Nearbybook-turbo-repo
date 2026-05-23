82f7587f46a848e56cb38791ce95e8dabeac52ad
ashdown0069 <kimjh0069@naver.com>
2026-05-23 21:42:29 +0900

    feat: main.ts에 swagger 초기화 코드 추가

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
 import helmet from "helmet"
 import { ValidationPipe } from "@nestjs/common"
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -17,6 +18,15 @@ async function bootstrap() {
       transform: true,
     })
   )
+
+  const config = new DocumentBuilder()
+    .setTitle("NearbyBook API")
+    .setDescription("The NearbyBook API description")
+    .setVersion("1.0")
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup("api-docs", app, document)
+
   console.log("port connected", process.env.PORT)
   await app.listen(process.env.PORT ?? 3000)
 }
