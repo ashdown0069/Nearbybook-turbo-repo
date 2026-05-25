d4b6ddc52c0ebf98c069acef678baa251b664f24
ashdown0069 <kimjh0069@naver.com>
2026-05-25 13:50:54 +0900

    feat(api): app.service 로깅 보완 누락분 추가

--- apps/api/src/app.service.ts
+++ apps/api/src/app.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from "@nestjs/common"
+import { Injectable, Logger } from "@nestjs/common"
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name)
+
   getHello(): string {
+    this.logger.log("Hello World API가 호출되었습니다.")
     return "Hello World!"
   }
 }
